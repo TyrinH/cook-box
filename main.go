55e09a2cb93dc54895c1ae8dab8896b894c2dfac
Tyrin Holloway <tychnk@gmail.com>
2024-02-13 18:44:44 -0600

    capturing steps for making the recipe now

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"strings"
 
 	"github.com/gocolly/colly"
@@ -16,6 +15,7 @@ type Recipe struct {
 	Author string
 	Link string
 	ImageUrl string
+	Steps []string
 }
 
 func main () {
@@ -26,13 +26,10 @@ func main () {
 
 	recipes := make([]Recipe, 0)
 
-
 	c.OnHTML("section", func(e *colly.HTMLElement) {
 		newRecipe := Recipe{}
 		newRecipe.Title = e.ChildText("h1.text-heading-1")
 		newRecipe.Descripition = e.ChildText("div.container.col-span-2.flex.flex-col.gap-8.py-8.lg\\:pr-10 > div.prose > h2")
-		imageUrl := e.ChildText("img")
-		log.Print("Image URL: ", imageUrl)
 		rawTags := e.ChildText("div.flex.flex-row.items-center.gap-1")
 		listOfSpans := e.ChildText("div.container.col-span-2.flex.flex-col.gap-8.py-8.lg\\:pr-10 > span")
 
@@ -46,21 +43,20 @@ func main () {
 				newRecipe.Tags = append(newRecipe.Tags, tagsSanitized[i])
 			}
 		}
-		
-		e.ForEach("li[data-testid=ingredient-group]", func(_ int, el *colly.HTMLElement) {
-			el.ForEach("ul > li > div.flex-grow > div > span", func(_ int, el *colly.HTMLElement) {
+			e.ForEach("ul > li > div.flex-grow > div > span", func(_ int, el *colly.HTMLElement) {
 				subIngredient := el.Text
 				newRecipe.Ingredients = append(newRecipe.Ingredients, subIngredient)
 			})
-		})
+			e.ForEach("div.grid.grid-cols-1.gap-8.lg\\:hidden > section.prose.flex-col > div", func(_ int, el *colly.HTMLElement) {
+				step := el.Text
+				newRecipe.Steps = append(newRecipe.Steps, step)
+			})
+
 		recipes = append(recipes, newRecipe)
 	})
 
 	c.OnRequest(func(r *colly.Request) {
 		fmt.Println("Visiting", r.URL.String())
 	})
-
-	// Start scraping on https://hackerspaces.org
 	c.Visit("https://www.cookwell.com/recipe/jalapeno-ranch-salad-w-adobo-chicken")
-
 }
